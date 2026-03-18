963d48d85bf77e63d25597a8551de85b7d92ff1a
pasrirahman111 <pastirahman0804@gmail.com>
2026-03-18 17:35:41 +0700

    Inisial commit/commit pertama

---
+++ database/migrations/2026_03_17_161735_create_buku_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul', 100);
+            $table->string('penulis', 100);
+            $table->string('penerbit', 100);
+            $table->year('tahun_terbit');
+            $table->integer('stok')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buku');
+    }
+};
